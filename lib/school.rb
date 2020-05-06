3d25734262847a43a9082a76bdf0e0225d38c7fa
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-06 13:33:32 -0600

    Complete school testing and code

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
+class School
+attr_reader :start_time,
+            :hours_in_school_day,
+            :student_names
 
+  def initialize (start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name (new_name)
+    @student_names << new_name
+  end
+
+  def end_time_conversion
+    @start_time.to_i + @hours_in_school_day
+  end
+
+  def end_time
+    "#{end_time_conversion}:00"
+  end
+
+
+end
